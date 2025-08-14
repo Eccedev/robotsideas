20402f6e80aeef56e155ac224e9a0ecce7409710
EcceDev <117386916+Eccedev@users.noreply.github.com>
2025-08-14 19:56:15 +0100

    Delete q-mensajes directory

--- q-mensajes/qu-relatos/qurelatos.js
+++
@@ -1,9 +0,0 @@
-const scrollBtn = document.getElementById("scroll-top-btn");
-        // Botón scroll arriba
-        window.addEventListener("scroll", () => {
-            scrollBtn.style.display = window.scrollY > 300 ? "block" : "none";
-        });
-
-        scrollBtn.onclick = () => {
-            window.scrollTo({ top: 0, behavior: "smooth" });
-        };
